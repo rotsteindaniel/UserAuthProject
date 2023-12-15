9bd67699c071b24319233735b57505da9a6faed0
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2023-12-15 10:02:45 -0300

    Refactor authentication logic in Edicao and Perfil pages

--- Frontend/src/app/(private)/edicao/page.tsx
+++ Frontend/src/app/(private)/edicao/page.tsx
@@ -1,9 +1,25 @@
-import styles from './page.module.css'
+import { useContext, useEffect } from "react";
+import styles from "./page.module.css";
+import { useRouter } from "next/router";
+import { AuthContext, AuthContextType } from "@/contexts/AuthContext";
 
 export default function Edicao() {
-  return (
-    <main className={styles.main}>
-      Pagina de edicao do usuário
-    </main>
-  )
+  const Router = useRouter();
+
+  const { user, isAuthenticated } = useContext<AuthContextType>(AuthContext);
+
+  useEffect(() => {
+    // Verifica se o usuário está autenticado
+    if (!isAuthenticated) {
+      // Se não estiver autenticado, redirecione para a página de login
+      Router.push("/login");
+    }
+  }, [isAuthenticated]);
+
+  // Se o usuário não estiver autenticado, evite a renderização do conteúdo da página
+  if (!isAuthenticated) {
+    return null;
+  }
+
+  return <main className={styles.main}>Pagina de edicao do usuário</main>;
 }
--- Frontend/src/app/(private)/perfil/page.tsx
+++ Frontend/src/app/(private)/perfil/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import styles from "./page.module.css";
 import { AuthContext, AuthContextType } from "@/contexts/AuthContext";
 import { useRouter } from "next/navigation";
@@ -8,7 +8,20 @@ import { useRouter } from "next/navigation";
 export default function Perfil() {
   const Router = useRouter();
 
-  const { user } = useContext<AuthContextType>(AuthContext);
+  const { user, isAuthenticated } = useContext<AuthContextType>(AuthContext);
+
+  useEffect(() => {
+    // Verifica se o usuário está autenticado
+    if (!isAuthenticated) {
+      // Se não estiver autenticado, redirecione para a página de login
+      Router.push("/login");
+    }
+  }, [isAuthenticated]);
+
+  // Se o usuário não estiver autenticado, evite a renderização do conteúdo da página
+  if (!isAuthenticated) {
+    return null;
+  }
 
   return (
     <main className={styles.main}>
