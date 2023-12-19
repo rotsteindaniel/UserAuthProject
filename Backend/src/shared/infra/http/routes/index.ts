aef9458419226c049b66f279e00d0e51feb37ac8
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2023-12-19 15:38:03 -0300

    Fix user routes import in index.ts

--- Backend/src/shared/infra/http/routes/index.ts
+++ Backend/src/shared/infra/http/routes/index.ts
@@ -5,7 +5,7 @@ import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
-router.use("/users", usersRoutes);
 router.use(authenticateRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
