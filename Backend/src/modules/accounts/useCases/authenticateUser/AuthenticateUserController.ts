6c04ad3327c232fe403eefb459fe45fa2bb960bb
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2023-12-21 08:40:44 -0300

    Refactor authentication controller

--- Backend/src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
+++ Backend/src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -1,21 +1,18 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
-import { z } from 'zod';
+import { z } from "zod";
 
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 import { BadRequestError } from "@shared/errors/ApiError";
 
 class AuthenticateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const authenticateBodySchema = z.object({
-      email: z.string().email(),
-      password: z.string().min(6),
-    });
-
     try {
-      const { email, password } = authenticateBodySchema.parse(request.body);
+      const { email, password } = request.body;
 
-      const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+      const authenticateUserUseCase = container.resolve(
+        AuthenticateUserUseCase
+      );
 
       const token = await authenticateUserUseCase.execute({
         email,
@@ -24,29 +21,11 @@ class AuthenticateUserController {
 
       return response.json(token);
     } catch (error) {
-      if (error instanceof z.ZodError) {
-        // Tratar erros de validação aqui
-        return response.status(400).json({
-          errors: error.errors.map((err) => ({
-            path: err.path.join('.'),
-            message: err.message,
-          })),
-        });
-      } else if (error instanceof BadRequestError) {
-        // Tratar erros específicos de negócios, se necessário
-        return response.status(400).json({
-          error: error.message,
-        });
-      } else {
-        // Outros tipos de erros, como erros internos do servidor
-        console.error(error);
-        return response.status(500).json({
-          error: "Internal Server Error",
-        });
+      if (error) {
+        throw new BadRequestError(error.message);
       }
     }
   }
 }
 
 export { AuthenticateUserController };
-
